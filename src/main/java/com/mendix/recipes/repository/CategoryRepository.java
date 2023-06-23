7bd12560459303c0b599e6464907eb329ba91af8
omer.yusufoglu@vodafone.com <omer.yusufoglu@vodafone.com>
2023-06-24 02:22:16 +0300

    add repository for category db operations

---
+++ src/main/java/com/mendix/recipes/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.mendix.recipes.repository;
+
+import com.mendix.recipes.model.Category;
+import com.mendix.recipes.model.Recipe;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * Repository class for crud operations
+ */
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+}
