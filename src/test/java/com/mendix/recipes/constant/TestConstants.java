05e2bcff615bd69851be60612a21640ba3adec84
omer.yusufoglu@vodafone.com <omer.yusufoglu@vodafone.com>
2023-06-24 02:26:10 +0300

    constants for test cases

---
+++ src/test/java/com/mendix/recipes/constant/TestConstants.java
@@ -0,0 +1,33 @@
+package com.mendix.recipes.constant;
+
+public class TestConstants {
+
+    private TestConstants() {
+    }
+
+    public static final Long RECIPE_ID = 12345634534545l;
+    public static final Long RECIPE_ID_TO_DELETE = 42345634534545l;
+
+    public static final String RECIPE_NAME = "RecipeTest";
+    public static final String RECIPE_NAME_TO_DELETE = "RecipeTestToDelete";
+
+    public static final String INSTRUCTIONS = "test instructions";
+    public static final String INSTRUCTIONS_TO_DELETE = "delete instructions";
+
+    public static final Integer NUMBER_OF_SERVING = 5;
+    public static final Integer NUMBER_OF_SERVING_TO_DELETE = 3;
+
+    public static final String INGREDIENT_NAME = "test ingredient";
+
+    public static final Long CATEGORY_ID = 4234534545l;
+    public static final String CATEGORY_NAME = "test category";
+
+    public static final String INGREDIENT_AMOUNT = "200g";
+
+    public static final String RECIPE_SEARCH_STRING = "title:*Recipe*";
+    public static final String RECIPE_SORT_BY = "name";
+
+    public static final Integer PAGE_NUMBER = 0;
+    public static final Integer PAGE_SIZE = 1;
+
+}
